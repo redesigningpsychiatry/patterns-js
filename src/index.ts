fe992dfe6f772a1b64840ec62069e149cab73b8a
Sander Voerman <sander@savoerman.nl>
2022-01-16 11:14:21 +0100

    Add interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
+export interface NodeCategory {
+  id: number;
+  name: string;
+  isBuildingBlock: boolean;
+}
+
+
+export interface RoleCategory extends NodeCategory {
+}
+
+
+export interface ContextCategory extends NodeCategory {
+  iconName: string;
+}
 
 
 export function hello(): string {
